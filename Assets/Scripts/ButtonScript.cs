e5513f2a31adc2a8fcf06bdfcc4cee84a1e86f10
king_spin <rubenjoossen@gmail.com>
2021-03-30 12:03:07 +0200

    colour change functionality

---
+++ Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonScript : MonoBehaviour
+{
+    public Text question;
+
+    public void OnY()
+    {
+        question.color = Color.green;
+    }
+
+    public void OnN()
+    {
+        question.color = Color.red;
+    }
+}
